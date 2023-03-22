753492ced6042e16d1e14cdaff5919a5ad386fd8
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-22 12:01:09 +0200

    Add comments

--- include/logger.hpp
+++ include/logger.hpp
@@ -6,18 +6,24 @@
 
 namespace my::log
 {
+    /// @brief Creates the instance of default logger to log to default logs
+    /// @return Logger
     static inline auto deflogger()
     {
         static auto instance(spdlog::basic_logger_mt("file-copy-shared-memory", "logs/deflogs.txt"));
         return instance;
     }
 
+    /// @brief Creates the instance of logger to log to producer logs
+    /// @return Logger
     static inline auto producer_logger()
     {
         static auto instance(spdlog::basic_logger_mt("file-copy-shared-memory", "logs/producer_logs.txt"));
         return instance;
     }
 
+    /// @brief Creates the instance of logger to log to consumer logs
+    /// @return Logger
     static inline auto consumer_logger()
     {
         static auto instance(spdlog::basic_logger_mt("file-copy-shared-memory", "logs/consumer_logs.txt"));
