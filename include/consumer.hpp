56ec73811b78775a097e35ff49e65c35ea60b7ca
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-22 10:50:01 +0200

    Implement consumer and producer

--- include/consumer.hpp
+++ include/consumer.hpp
@@ -4,38 +4,89 @@
 
 #include <fstream>
 #include <iostream>
+#include <queue>
 
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/sync/named_condition.hpp>
 #include <boost/interprocess/sync/named_mutex.hpp>
 
-#include "config.hpp"
+#include "buffer.hpp"
+#include "shared_memory.hpp"
+#include "logger.hpp"
 
 namespace inter
 {
+    namespace bi = boost::interprocess;
 
     class consumer
     {
     public:
         consumer(const std::string &out_filepath)
-            : _filepath{out_filepath}
+            : _outFile{out_filepath, std::ios_base::binary}
         {
         }
 
         inline void run()
         {
-            std::ofstream outFile{_filepath, std::ios_base::binary};
-
-            if (!outFile)
+            if (!_outFile)
             {
-                std::cout << "Cannot run copping because output filepath is invalid!\n";
+                my::log::logger()->error("[Consumer] Cannot run copping because output filepath is invalid!");
                 return;
             }
+
+            _startProducer = shm::getStartProducer();
+
+            _endConsumer = shm::getEndConsumer();
+
+            while (*_startProducer == false)
+            {
+                my::log::logger()->info("[Consumer] Waiting for staring producer process...");
+            }
+
+            _remainedSymbols = shm::getRemainedSymbols();
+
+            _bufferQueue = shm::getBufferQueue();
+
+            writeToFile();
+
+            my::log::logger()->info("[Consumer] Consuming done!");
         }
 
     private:
-        std::string _filepath;
+        inline void writeToFile()
+        {
+            my::log::logger()->flush();
+            my::log::logger()->info("[Consumer] Start processing...");
 
-        std::size_t _remainedSymbols{};
+            while (*_remainedSymbols > 0)
+            {
+                if (_bufferQueue->readSize > 0)
+                {
+                    bi::scoped_lock lk(_mtx);
+
+                    _outFile.write(_bufferQueue->data, _bufferQueue->readSize);
+
+                    *_remainedSymbols -= _bufferQueue->readSize;
+                    _bufferQueue->readSize = 0;
+                }
+            }
+
+            *_endConsumer = true;
+        }
+
+    private:
+        std::ofstream _outFile;
+
+        std::size_t *_remainedSymbols{nullptr};
+
+        buffer *_bufferQueue{nullptr};
+        static constexpr uint32_t _sizeOfBufferQueue{100};
+
+        bool *_startProducer{nullptr};
+        bool *_endConsumer{nullptr};
+
+        bi::named_mutex _mtx{bi::open_or_create, shm::MutexName};
+        bi::named_condition _condVar{bi::open_or_create, shm::CondVarName};
     };
 
 } // namespace inter
--- include/producer.hpp
+++ include/producer.hpp
@@ -4,38 +4,99 @@
 
 #include <fstream>
 #include <iostream>
+#include <queue>
 
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/sync/named_condition.hpp>
 #include <boost/interprocess/sync/named_mutex.hpp>
 
-#include "config.hpp"
+#include "buffer.hpp"
+#include "shared_memory.hpp"
+#include "logger.hpp"
 
 namespace inter
 {
+    namespace bi = boost::interprocess;
 
     class producer
     {
     public:
         producer(const std::string &in_filepath)
-            : _filepath{in_filepath}
+            : _inFile{in_filepath, std::ios_base::binary}
         {
+            if (_inFile)
+            {
+                _remainedSymbols = new std::size_t(std::filesystem::file_size(in_filepath));
+            }
+        }
+
+        ~producer()
+        {
+            shm::remove();
         }
 
         inline void run()
         {
-            std::ifstream inFile{_filepath, std::ios_base::binary};
-
-            if (!inFile)
+            if (!_inFile)
             {
-                std::cout << "Cannot run copping because input filepath is invalid!\n";
+                my::log::logger()->error("[Producer] Cannot run copping because input filepath is invalid!");
                 return;
             }
 
-            const std::size_t remainedSymbols = std::filesystem::file_size(_filepath);
+            _startProducer = shm::getStartProducer();
+
+            _endConsumer = shm::getEndConsumer();
+
+            std::size_t value = *_remainedSymbols;
+
+            delete _remainedSymbols;
+            _remainedSymbols = shm::setRemainedSymbols(value);
+
+            _bufferQueue = shm::setBufferQueue();
+
+            readFromFile();
+
+            my::log::logger()->info("[Producer] Producing done!");
         }
 
     private:
-        std::string _filepath;
+        inline void readFromFile()
+        {
+            *_startProducer = true;
+
+            my::log::logger()->flush();
+            my::log::logger()->info("[Producer] Start processing...");
+
+            while (_inFile)
+            {
+                if (_bufferQueue->readSize == 0)
+                {
+                    bi::scoped_lock lk(_mtx);
+
+                    _inFile.read(_bufferQueue->data, _bufferQueue->bufferSize);
+                    _bufferQueue->readSize = _inFile.gcount();
+                }
+            }
+
+            while (*_endConsumer == false)
+            {
+                my::log::logger()->info("[Producer] Waiting for ending consumer process...");
+            }
+        }
+
+    private:
+        std::ifstream _inFile;
+
+        std::size_t *_remainedSymbols{nullptr};
+
+        buffer *_bufferQueue{nullptr};
+        static constexpr uint32_t _sizeOfBufferQueue{100};
+
+        bool *_startProducer{nullptr};
+        bool *_endConsumer{nullptr};
+
+        bi::named_mutex _mtx{bi::open_or_create, shm::MutexName};
+        bi::named_condition _condVar{bi::open_or_create, shm::CondVarName};
     };
 
 } // namespace inter
