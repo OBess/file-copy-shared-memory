f8c2145d3c440034fe9a262785b9933652a58e16
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-24 16:47:27 +0200

    Change pathes

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-start "Process 1" %~dp0\file-copy-shared-memory.exe --in="C:\My\Projects\cpp\file-copy-shared-memory\deploy\Debug\Itou_Junj_Collection_[01]_[AniLibria_TV]_[HDTV-Rip_720p].mkv" --out="C:\My\Projects\cpp\file-copy-shared-memory\deploy\Debug\output.mkv" --name="FILE_COPY_SHARED"
+start "Process 1" %~dp0\file-copy-shared-memory.exe --in="your.file" --out="output.file" --name="FILE_COPY_SHARED"
 
-start "Process 2" %~dp0\file-copy-shared-memory.exe --in="C:\My\Projects\cpp\file-copy-shared-memory\deploy\Debug\Itou_Junj_Collection_[01]_[AniLibria_TV]_[HDTV-Rip_720p].mkv" --out="C:\My\Projects\cpp\file-copy-shared-memory\deploy\Debug\output.mkv" --name="FILE_COPY_SHARED"
+start "Process 2" %~dp0\file-copy-shared-memory.exe --in="your.file" --out="output.file" --name="FILE_COPY_SHARED"
 
 @ECHO ON
