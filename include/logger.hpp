14ff0d4f5c2a58fce1b47c6a045a0e2f5dd852d6
Dmytro Sushchenko <34779566+OBess@users.noreply.github.com>
2023-03-28 14:44:13 +0300

    Set custom pattern for logger

--- include/logger.hpp
+++ include/logger.hpp
@@ -2,14 +2,24 @@
 #ifndef __INCLUDE_LOGGER_HPP__
 #define __INCLUDE_LOGGER_HPP__
 
+#include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
 namespace my::log
 {
+    /// @brief Sets the pattern to loggers
+    /// @return For IIFE idiom
+    static inline uint8_t set_pattern()
+    {
+        spdlog::set_pattern("[%Y-%M-%d %H:%M:%S] [pid %P] %v");
+        return 0;
+    }
+
     /// @brief Creates the instance of default logger to log to default logs
     /// @return Logger
     static inline auto deflogger()
     {
+        static auto setPattern = std::invoke(set_pattern);
         static auto instance(spdlog::basic_logger_mt("file-copy-shared-memory", "logs/deflogs.txt"));
         return instance;
     }
